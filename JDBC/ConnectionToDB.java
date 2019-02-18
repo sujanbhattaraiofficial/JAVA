c6be4e996baa410192a64296e8049cfb9dac50e2
Sujan Bhattarai <sujanb681@gmail.com>
2019-02-18 17:07:45 +0545

    Connection To DB file upload

---
+++ JDBC/ConnectionToDB.java
@@ -0,0 +1,19 @@
+public class ConnetionToBD
+{
+    public static void main(String[] args) {
+        Connection myConn = null;
+       
+		
+			try {
+				myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/gces","root","");
+				System.out.println("Sucessfully Connected");
+				
+			
+				
+			} catch (SQLException e1) 
+			{
+				System.out.println("error");
+			}
+			
+    }
+}
